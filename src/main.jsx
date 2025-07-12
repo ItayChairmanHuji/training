69bc5d1f6ef464f9bc54a92fe24e1bf4add2b0ec
Itay <test@test.com>
2025-07-12 14:21:17 +0300

    let's try it

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DLInterviewTrainer from "./App";
+import DLInterviewTrainer from "./DLInterviewTrainer";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DLInterviewTrainer />
   </React.StrictMode>
